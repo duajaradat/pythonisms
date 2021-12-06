b5b3b98bc64fd1a4a3611fe7d9a26538326cd19c
duajaradat <duajaradat164@gmail.com>
2021-12-06 17:11:07 +0200

    test for pythonisms

--- pythonisms/pythonisms.py
+++ pythonisms/pythonisms.py
@@ -3,7 +3,7 @@ class Node:
         self.value = value
         self.next = next
 
-class linkedList:
+class LinkedList:
     def __init__(self, collection=None):
         self.head = None
         if collection :
@@ -27,7 +27,7 @@ class linkedList:
         for i, item in enumerate(self.head):
             if i == index:
                 return item
-        
+
     def __iter__(self):
         def  generator():
             current = self.head
